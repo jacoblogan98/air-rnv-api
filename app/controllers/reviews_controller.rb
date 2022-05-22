06a7597225e66ec5b4dd6d94b4e5bf334e21cc62
Mark <mark.n.bacon.jr@gmail.com>
2022-05-22 02:44:27 -0500

    fixed routes for CR(u)D

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,26 @@ class ReviewsController < ApplicationController
         reviews = Review.all
         render json: reviews, include: ['user'], status: :ok
     end
+
+    def show
+        review = Review.find(params[:id])
+        render json: review, status: :ok
+    end
+
+    def create
+        review = Review.create!(review_params)
+        render json: review, status: :ok
+    end
+
+    def destroy
+        review = Review.find(params[:id])
+        review.destroy
+        render json: {}, status: :ok
+        head :no_content
+    end
+    private
+
+    def review_params
+        params.permit(:content, :rating, :user_id, :rv_id)
+    end
 end
--- app/serializers/rv_serializer.rb
+++ app/serializers/rv_serializer.rb
@@ -1,6 +1,7 @@
 class RvSerializer < ActiveModel::Serializer
   attributes :id, :name, :occupancy, :location, :day_rate, :pet_friendly, :air_conditioned, :shower, :tv, :description, :image
 
+  
   def image
     self.object.photos.first.url
   end
